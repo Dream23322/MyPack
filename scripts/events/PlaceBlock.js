cf4d8e6941c58e97a3ceaf4be74bc13645753824
Dream23322 <27ellisj@mbbc.qld.edu.au>
2025-06-08 20:15:16 +1000

    Refactor PlaceBlock event handling

--- scripts/events/PlaceBlock.js
+++ scripts/events/PlaceBlock.js
@@ -1,6 +1,10 @@
 import * as mc from "@minecraft/server";
+import { IBlock } from "../api/classes/IBlock";
 
-mc.world.beforeEvents.playerPlaceBlock.subscribe((eventData) => {
-    
+mc.world.afterEvents.playerPlaceBlock.subscribe((eventData) => {
+    const player = eventData.player;
+    const block = eventData.block
+    const iblock = new IBlock(block.location.x, block.location.y, block.location.z, block.dimension.id);
+
     
 });
