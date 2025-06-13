9a784bf2562d4313e294cda2f900cb2ed3dd6d06
Dream23322 <27ellisj@mbbc.qld.edu.au>
2025-06-13 11:01:22 +1000

    Move placeblock into the api

--- scripts/api/events/PlaceBlock.js
+++ scripts/api/events/PlaceBlock.js
@@ -1,5 +1,5 @@
 import * as mc from "@minecraft/server";
-import { IBlock } from "../api/classes/IBlock";
+import { IBlock } from "../classes/IBlock";
 
 /**
  * @event playerPlaceBlock - Before Event
